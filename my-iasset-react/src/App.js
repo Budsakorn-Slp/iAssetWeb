a2963782a531dd8cf6c07d6259f904f3ceaa191a
Budsakornsil <90294388+Budsakorn-Slp@users.noreply.github.com>
2023-11-07 17:36:09 +0700

    commit

--- my-iasset-react/src/App.js
+++ my-iasset-react/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
+import Home from './components/features/home/home';
+import Borrow from '../src/components/features/userManage/borrow/borrow'
+import Return from '../src/components/features/userManage/return/return'
+import CrudUser from "./components/features/settingAdmin/crudUser/crudUser";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <div>
+            <Home />
+          </div>
+        } />
+        <Route path="/home" element={<Home />} />
+        <Route path="/borrow" element={<Borrow />} />
+        <Route path="/return" element={<Return />} />
+        <Route path="/userCreate" element={<CrudUser />} />
+      </Routes>
+    </Router>
   );
 }
 
